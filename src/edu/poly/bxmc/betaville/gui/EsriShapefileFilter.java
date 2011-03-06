7d5933367fce0689fc5caaa92b1ab101198fc3ce
skye.book <skye.book@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2011-03-06 21:26:23 +0000

    Added a Java FileFilter for Esri Shapefiles
    git-svn-id: https://betaville.googlecode.com/svn/BetavilleApp/trunk@3454 8c047b66-80f2-bdae-70f3-23be71ff3dc6

---
+++ src/edu/poly/bxmc/betaville/gui/EsriShapefileFilter.java
@@ -0,0 +1,58 @@
+/** Copyright (c) 2008-2011, Brooklyn eXperimental Media Center
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Brooklyn eXperimental Media Center nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Brooklyn eXperimental Media Center BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.poly.bxmc.betaville.gui;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+/**
+ * {@link FileFilter} which shows only directories and Esri Shapefiles
+ * @author Skye Book
+ *
+ */
+public class EsriShapefileFilter extends FileFilter {
+
+	/*
+	 * (non-Javadoc)
+	 * @see javax.swing.filechooser.FileFilter#getDescription()
+	 */
+	@Override
+	public String getDescription() {
+		return "Esri Shapefile";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see javax.swing.filechooser.FileFilter#accept(java.io.File)
+	 */
+	@Override
+	public boolean accept(File f) {
+		if(f.toString().endsWith("shp")||f.isDirectory()) return true;
+		else return false;
+	}
+
+}
