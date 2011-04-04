63e917837c8aa44a9d0ea43440495ad377532ad2
skye.book <skye.book@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2011-04-04 16:29:37 +0000

    Fix for the delete_cache batch file not deleting the cache folder
    git-svn-id: https://betaville.googlecode.com/svn/BetavilleApp/trunk@3537 8c047b66-80f2-bdae-70f3-23be71ff3dc6

--- data/utils/delete_cache.bat
+++ data/utils/delete_cache.bat
@@ -2,7 +2,6 @@ REM delete_cache.bat - Batch file to delete Betaville cache directory
 REM This file has almost as many comments as commands!
 REM by Skye Book <skye.book@gmail.com>
 
-chdir %USERPROFILE%\.betaville
-rd cache /s /q
+rd "%USERPROFILE%\.betaville\cache" /s /q
 @echo "Your Betaville cache files have been deleted!"
 pause
