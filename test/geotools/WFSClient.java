88a6ec02bafc519bc3cb59379ed9a08a1a40bf91
skye.book <skye.book@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2011-05-11 20:47:22 +0000

    Added more example code to the WFSClient test and updated the build file to include the GeoTools jars
    git-svn-id: https://betaville.googlecode.com/svn/BetavilleApp/trunk@3666 8c047b66-80f2-bdae-70f3-23be71ff3dc6

--- test/geotools/WFSClient.java
+++ test/geotools/WFSClient.java
@@ -19,13 +19,24 @@ import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.simple.SimpleFeatureTypeImpl;
 import org.geotools.geometry.jts.JTS;
 import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.CRS;
 import org.opengis.feature.Feature;
+import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.filter.FilterFactory2;
 import org.opengis.filter.spatial.Intersects;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.EmptyGeometry;
 import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 
 import edu.poly.bxmc.betaville.net.wfs.WFSParams;
 
@@ -36,10 +47,13 @@ import edu.poly.bxmc.betaville.net.wfs.WFSParams;
 public class WFSClient {
 	
 	private String server = "http://192.168.1.6:8080/geoserver/";
+	
+	private CoordinateReferenceSystem epsg4326;
+	private MathTransform transform;
 
-	public WFSClient() throws IOException{
+	public WFSClient() throws IOException, NoSuchAuthorityCodeException, FactoryException, TransformException{
 		
-		
+		epsg4326 = CRS.decode("EPSG:4326");
 		
 		String getCapabilities = server+"wfs?REQUEST=GetCapabilities";
 
@@ -57,9 +71,11 @@ public class WFSClient {
 			}
 		}
 		String typeName = typeNames[0];
+		typeName = "Betaville:DOITT_SUBWAY_ENTRANCE_01_13SEPT2010";
 		SimpleFeatureTypeImpl schema = (SimpleFeatureTypeImpl) data.getSchema(typeName);
 
 		// Step 4 - target
+		// reaches out to the network
 		FeatureSource<SimpleFeatureType, SimpleFeature> source = data.getFeatureSource(typeName);
 		
 		
@@ -76,13 +92,31 @@ public class WFSClient {
 		//Query query = new DefaultQuery(typeName, filter, new String[]{geomName});
 		Query query = new DefaultQuery(typeName);
 		FeatureCollection<SimpleFeatureType, SimpleFeature> features = source.getFeatures(query);
-
+		
 		ReferencedEnvelope bounds = new ReferencedEnvelope();
 		Iterator<SimpleFeature> iterator = features.iterator();
 		try {
 			while(iterator.hasNext()){
-				Feature feature = (Feature) iterator.next();
+				SimpleFeature feature = (SimpleFeature) iterator.next();
+				System.out.println("Feature: " + feature.getIdentifier().getID());
 				bounds.include(feature.getBounds());
+				if(feature.getDefaultGeometry() instanceof Point){
+					Point point = (Point) feature.getDefaultGeometry();
+					//System.out.println("SRID: "+point.getSRID());
+					transform = CRS.findMathTransform(feature.getType().getCoordinateReferenceSystem(), epsg4326);
+					Coordinate latLon = JTS.transform(point.getCoordinate(), null, transform);
+					System.out.println(latLon.toString());
+				}
+				else if(feature.getDefaultGeometry() instanceof Polygon){
+					
+				}
+				else if(feature.getDefaultGeometry() instanceof EmptyGeometry){
+					
+				}
+				System.out.println(feature.getDefaultGeometry().getClass().getName());
+				for(Property p : feature.getProperties()){
+					System.out.println("\t"+p.getType().getName());
+				}
 			}
 			System.out.println("Calculated Bounds:"+ bounds);
 		}
@@ -91,7 +125,7 @@ public class WFSClient {
 		}
 	}
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, NoSuchAuthorityCodeException, FactoryException, TransformException{
 		long start = System.currentTimeMillis();
 		new WFSClient();
 		System.out.println("Request took " + (System.currentTimeMillis()-start));
