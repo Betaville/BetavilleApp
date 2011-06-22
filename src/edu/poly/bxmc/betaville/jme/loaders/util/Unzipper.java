1e5690f8797550b036b200c659884c78dfc42dde
skye.book <skye.book@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2011-06-22 23:51:12 +0000

    Fixed the unzipper utility
    git-svn-id: https://betaville.googlecode.com/svn/BetavilleApp/trunk@3743 8c047b66-80f2-bdae-70f3-23be71ff3dc6

--- src/edu/poly/bxmc/betaville/jme/loaders/util/Unzipper.java
+++ src/edu/poly/bxmc/betaville/jme/loaders/util/Unzipper.java
@@ -1,4 +1,4 @@
-/** Copyright (c) 2008-2010, Brooklyn eXperimental Media Center
+/** Copyright (c) 2008-2011, Brooklyn eXperimental Media Center
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -29,120 +29,49 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 /**
- * <code>Unzipper</code> provides a static method to unzip a file.
+ * Provides a static method to unzip an archive.
  * @author Skye Book
  *
  */
 public class Unzipper {
-	public static void unzip(URL modelURL)
-	{
-		ZipEntry zipEntry;
-		try {
-			File kmzFile = new File(modelURL.toURI());
-			File dataDir = new File("data/" + kmzFile.getName().subSequence(0, kmzFile.getName().length()-3) + "/");
-			dataDir.mkdir();
-			FileInputStream fileIn = new FileInputStream(kmzFile);
-			ZipInputStream zipIn = new ZipInputStream(new BufferedInputStream(fileIn));
-			while((zipEntry = zipIn.getNextEntry())!=null)
-			{
-				String entryName = zipEntry.getName();
+	private static final int BUFFER_SIZE = 2048;
 
-				// handle missing directories -- PUT IN FUNCTION
-				System.out.println("entry name: " + entryName);
-				if(entryName.contains("/"))
-				{
-					// make directory
-					//File dirToMake = new File();
-					System.out.println("dealing with directory");
-					String[] directories = zipEntry.getName().split("/");
-					// iterate through the directories array with the
-					// exception of the last result (which is the filename)
-					for(int i=0; i<(directories.length-1);i++)
-					{
-						System.out.println("creating directory: " + directories[i]);
-						File newDir = new File(directories[i]);
-						newDir.mkdir();
-						System.out.println("dir done");
-					}
-				}
-				else{
-					// make file
-				}
-
-				//END DIRECTORY HANDLING
-
-				int size;
-				byte[] buffer = new byte[1024];
-
-
-				FileOutputStream fileOut = new FileOutputStream(dataDir.getAbsolutePath()+zipEntry.getName());
-				BufferedOutputStream bufferedOut = new BufferedOutputStream(fileOut, buffer.length);
-
-
-
-
-				while((size=zipIn.read(buffer,0,buffer.length)) != -1)
-				{
-					bufferedOut.write(buffer,0,size);
-				}
-
-				bufferedOut.flush();
-				bufferedOut.close();
-
-			}
-			zipIn.close();
-			fileIn.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args){
-		/*
-		try {
-			unzip(new File("data/test.zip").toURI().toURL());
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 */
-
-
-		int BUFFER = 2048;
+	/**
+	 * Decompresses a zip archive
+	 * @param file The zip file to decompress
+	 * @param destinationFolder The file to send the extracted files to.  A null argument will cause the file to be unzipped in its location
+	 */
+	public static void unzip(File file, File destinationFolder){
 		try {
 			BufferedOutputStream dest = null;
+			if(destinationFolder==null) destinationFolder = new File(file.toString().substring(0, file.toString().indexOf(".")));
+			else destinationFolder = new File(destinationFolder+"/"+file.toString().substring(file.toString().lastIndexOf("/")+1, file.toString().indexOf(".")));
+			System.out.println("folder: " + destinationFolder);
+			if(!destinationFolder.exists()){
+				destinationFolder.mkdirs();
+			}
 			FileInputStream fis = new 
-			FileInputStream(new File("data/test.zip"));
+			FileInputStream(file);
 			ZipInputStream zis = new 
 			ZipInputStream(new BufferedInputStream(fis));
 			ZipEntry entry;
 			while((entry = zis.getNextEntry()) != null) {
 				System.out.println("Extracting: " +entry);
 				int count;
-				byte data[] = new byte[BUFFER];
+				byte data[] = new byte[BUFFER_SIZE];
 				// write the files to the disk
 				if(!entry.getName().endsWith(".DS_Store") && !entry.getName().startsWith("__MACOSX")){
-					FileOutputStream fos = new 
-					FileOutputStream(entry.getName());
+					File localFolder = new File(destinationFolder+"/"+entry.getName().substring(0, entry.getName().indexOf("/")+1));
+					localFolder.mkdirs();
+					FileOutputStream fos = new FileOutputStream(destinationFolder+"/"+entry.getName());
 					dest = new 
-					BufferedOutputStream(fos, BUFFER);
-					while ((count = zis.read(data, 0, BUFFER)) 
+					BufferedOutputStream(fos, BUFFER_SIZE);
+					while ((count = zis.read(data, 0, BUFFER_SIZE)) 
 							!= -1) {
 						dest.write(data, 0, count);
 					}
