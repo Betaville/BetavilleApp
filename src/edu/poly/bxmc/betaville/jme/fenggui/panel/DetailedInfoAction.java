7ab668178d6f459cb104ed92e97db06ac523c165
skye.book <skye.book@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2011-08-22 14:33:23 +0000

    Added location display to the Details window
    git-svn-id: https://betaville.googlecode.com/svn/BetavilleApp/trunk@3896 8c047b66-80f2-bdae-70f3-23be71ff3dc6

--- src/edu/poly/bxmc/betaville/jme/fenggui/panel/DetailedInfoAction.java
+++ src/edu/poly/bxmc/betaville/jme/fenggui/panel/DetailedInfoAction.java
@@ -26,15 +26,25 @@
 package edu.poly.bxmc.betaville.jme.fenggui.panel;
 
 import org.apache.log4j.Logger;
+import org.fenggui.Container;
 import org.fenggui.FengGUI;
 import org.fenggui.Label;
+import org.fenggui.RadioButton;
+import org.fenggui.ToggableGroup;
 import org.fenggui.composite.Window;
+import org.fenggui.event.ISelectionChangedListener;
+import org.fenggui.event.SelectionChangedEvent;
 import org.fenggui.layout.RowExLayout;
+import org.fenggui.layout.RowExLayoutData;
 
 import com.jme.scene.Spatial;
 
 import edu.poly.bxmc.betaville.SceneScape;
+import edu.poly.bxmc.betaville.jme.fenggui.extras.GPSView;
 import edu.poly.bxmc.betaville.jme.fenggui.extras.IBetavilleWindow;
+import edu.poly.bxmc.betaville.jme.fenggui.extras.LocationView;
+import edu.poly.bxmc.betaville.jme.fenggui.extras.OpenGLView;
+import edu.poly.bxmc.betaville.jme.fenggui.extras.UTMView;
 import edu.poly.bxmc.betaville.jme.intersections.ISpatialSelectionListener;
 import edu.poly.bxmc.betaville.model.Design;
 
@@ -55,6 +65,30 @@ public class DetailedInfoAction extends Window implements IBetavilleWindow {
 	private String filePfx = "File: ";
 	private String uploadedByPfx = "Uploaded By: ";
 	
+	
+	
+	// check current item location:
+	private Container locationContainer;
+	private Container selectorContainer;
+	
+	private ToggableGroup<RadioButton<Boolean>> togglableGroup;
+	private RadioButton<Boolean> latLon;
+	private RadioButton<Boolean> utm;
+	private RadioButton<Boolean> ogl;
+	
+	private static final String latLonSelection = "Lat/Lon";
+	private static final String utmSelection = "UTM";
+	private static final String openGLSelection = "OpenGL Units";
+
+	private GPSView gpsView;
+	
+	private UTMView utmView;
+
+	private OpenGLView oglView;
+	
+	
+	private boolean empty=true;
+	
 	/**
 	 * 
 	 */
@@ -65,21 +99,97 @@ public class DetailedInfoAction extends Window implements IBetavilleWindow {
 		file = FengGUI.createWidget(Label.class);
 		uploadedBy = FengGUI.createWidget(Label.class);
 		
+		locationContainer = FengGUI.createWidget(Container.class);
+		locationContainer.setLayoutManager(new RowExLayout(false));
+		
+		gpsView = new GPSView();
+		gpsView.setTitle("Point 1");
+		gpsView.setLayoutData(new RowExLayoutData(true, true));
+		
+		utmView = new UTMView();
+		utmView.setTitle("Point 1");
+		utmView.setLayoutData(new RowExLayoutData(true, true));
+		
+		oglView = new OpenGLView();
+		oglView.setTitle("Point 1");
+		oglView.setLayoutData(new RowExLayoutData(true, true));
+		
+		selectorContainer = FengGUI.createWidget(Container.class);
+		selectorContainer.setLayoutManager(new RowExLayout(true));
+		
+		togglableGroup = new ToggableGroup<RadioButton<Boolean>>();
+	    latLon = FengGUI.createRadioButton();
+	    latLon.setLayoutData(new RowExLayoutData(true, true));
+	    latLon.setRadioButtonGroup(togglableGroup);
+	    latLon.setText(latLonSelection);
+	    
+	    utm = FengGUI.createRadioButton();
+	    utm.setLayoutData(new RowExLayoutData(true, true));
+	    utm.setRadioButtonGroup(togglableGroup);
+	    utm.setText(utmSelection);
+	    
+	    ogl = FengGUI.createRadioButton();
+	    ogl.setLayoutData(new RowExLayoutData(true, true));
+	    ogl.setRadioButtonGroup(togglableGroup);
+	    ogl.setText(openGLSelection);
+	    
+	    latLon.setSelected(true);
+	    selectorContainer.addWidget(latLon, utm, ogl);
+	    
+	    locationContainer.addWidget(selectorContainer, gpsView);
+	    
+	    togglableGroup.addSelectionChangedListener(new ISelectionChangedListener() {
+			
+			public void selectionChanged(Object arg0, SelectionChangedEvent arg1) {
+				// remove widgets from location container
+				locationContainer.removeAllWidgets();
+				
+				locationContainer.addWidget(selectorContainer);
+				// add the widgets based on the selection
+				if(togglableGroup.getSelectedItem().equals(latLon)){
+					locationContainer.addWidget(gpsView);
+				}
+				else if(togglableGroup.getSelectedItem().equals(utm)){
+					locationContainer.addWidget(utmView);
+				}
+				else if(togglableGroup.getSelectedItem().equals(ogl)){
+					locationContainer.addWidget(oglView);
+				}
+				
+				layout();
+				
+			}
+		});
+
+		
+		
 		SceneScape.addSelectionListener(new ISpatialSelectionListener() {
 			
 			public void selectionCleared(Design previousDesign) {
 				getContentContainer().removeAllWidgets();
+				empty=true;
 			}
 			
 			public void designSelected(Spatial spatial, Design design) {
 				id.setText(idPfx+design.getID());
 				file.setText(filePfx+design.getFilepath());
 				uploadedBy.setText(uploadedByPfx+design.getUser());
-				if(!id.isInWidgetTree())getContentContainer().addWidget(id, file, uploadedBy);
+				
+				// update the location views
+				gpsView.updateLocation(design.getCoordinate().getGPS());
+				utmView.updateLocation(design.getCoordinate());
+				oglView.updateLocation(spatial.getLocalTranslation());
+				
+				
+				if(empty){
+					getContentContainer().addWidget(id, file, uploadedBy, locationContainer);
+					empty=false;
+				}
 				layout();
 			}
 		});
 	}
+	
 	
 	/* (non-Javadoc)
 	 * @see edu.poly.bxmc.betaville.jme.fenggui.extras.IBetavilleWindow#finishSetup()
