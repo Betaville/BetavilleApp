86b9f9dee47879155684b8cc897ad6c3f34e4cf8
skye.book <skye.book@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2011-11-18 00:43:01 +0000

    Added SunSyncModule for synchronizing the default light sources to the sun
    git-svn-id: https://betaville.googlecode.com/svn/BetavilleApp/trunk@4027 8c047b66-80f2-bdae-70f3-23be71ff3dc6

---
+++ src/edu/poly/bxmc/betaville/weather/SunSyncModule.java
@@ -0,0 +1,151 @@
+/** Copyright (c) 2008-2011, Brooklyn eXperimental Media Center
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Brooklyn eXperimental Media Center nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Brooklyn eXperimental Media Center BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package edu.poly.bxmc.betaville.weather;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+import org.geotools.nature.SunRelativePosition;
+
+import com.jme.light.DirectionalLight;
+import com.jme.math.Quaternion;
+import com.jme.math.Vector3f;
+import com.jme.scene.Node;
+import com.jme.scene.state.LightState;
+import com.jme.scene.state.RenderState.StateType;
+
+import edu.poly.bxmc.betaville.jme.gamestates.ShadowPassState;
+import edu.poly.bxmc.betaville.jme.map.GPSCoordinate;
+import edu.poly.bxmc.betaville.jme.map.JME2MapManager;
+import edu.poly.bxmc.betaville.jme.map.Rotator;
+import edu.poly.bxmc.betaville.module.GlobalSceneModule;
+import edu.poly.bxmc.betaville.module.Module;
+
+/**
+ * Synchronizes light sources to the sun's position
+ * @author Skye Book
+ *
+ */
+public class SunSyncModule extends Module implements GlobalSceneModule {
+	
+	protected GPSCoordinate coordinate;
+	
+	private SunRelativePosition sunRelativePosition;
+	
+	protected Date dateTime;
+	
+	private float[] vector = new float[3];
+	private Vector3f sunAngle = new Vector3f(0, 0, 0);
+	
+	private boolean realTime = true;
+	
+	private GregorianCalendar calendar;
+
+	/**
+	 * @param name
+	 */
+	public SunSyncModule(String name) {
+		super(name);
+		sunRelativePosition = new SunRelativePosition();
+		dateTime = new Date(System.currentTimeMillis());
+		calendar = new GregorianCalendar(TimeZone.getDefault());
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		dateTime = calendar.getTime();
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.poly.bxmc.betaville.module.SceneModule#initialize(com.jme.scene.Node)
+	 */
+	@Override
+	public void initialize(Node scene) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.poly.bxmc.betaville.module.SceneModule#onUpdate(com.jme.scene.Node, com.jme.math.Vector3f, com.jme.math.Vector3f)
+	 */
+	@Override
+	public void onUpdate(Node scene, Vector3f cameraLocation,
+			Vector3f cameraDirection) {
+		
+		if(realTime) dateTime.setTime(System.currentTimeMillis());
+		else{
+			dateTime = calendar.getTime();
+		}
+		
+		// presuming the scene's two default lights
+		LightState lightState = (LightState) scene.getRenderState(StateType.Light);
+		
+		coordinate = JME2MapManager.instance.betavilleToUTM(cameraLocation).getGPS();
+		
+		sunRelativePosition.setDate(dateTime);
+		sunRelativePosition.setCoordinate(coordinate.getLongitude(), coordinate.getLatitude());
+		double azimuth = sunRelativePosition.getAzimuth();
+		double elevation = sunRelativePosition.getElevation();
+		Quaternion q = Rotator.angleY((float)azimuth);
+		q.addLocal(Rotator.angleX((float)elevation));
+		q.toAngles(vector);
+		sunAngle.x=vector[0];
+		sunAngle.y=vector[1];
+		sunAngle.z=vector[2];
+		
+		((DirectionalLight)lightState.get(0)).setDirection(sunAngle);
+		((DirectionalLight)lightState.get(1)).setDirection(sunAngle.negate());
+		ShadowPassState.getInstance().getMapPass().setDirection(sunAngle);
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.poly.bxmc.betaville.module.IModule#deconstruct()
+	 */
+	@Override
+	public void deconstruct() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public GPSCoordinate getCoordinate() {
+		return coordinate;
+	}
+
+	public void setCoordinate(GPSCoordinate coordinate) {
+		this.coordinate = coordinate;
+	}
+
+	public boolean isRealTime() {
+		return realTime;
+	}
+
+	public void setRealTime(boolean realTime) {
+		this.realTime = realTime;
+	}
+
+	public GregorianCalendar getCalendar() {
+		return calendar;
+	}
+
+}
