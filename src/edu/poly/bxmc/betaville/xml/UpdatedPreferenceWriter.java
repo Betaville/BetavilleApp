b907e9d602a44567b47c3f0110dbdb14908da39f
clem.paulsen <clem.paulsen@8c047b66-80f2-bdae-70f3-23be71ff3dc6>
2015-03-31 19:49:36 +0000

    Correct text on thumbnail issue. Removing comments.
    git-svn-id: https://betaville.googlecode.com/svn/BetavilleApp/trunk@4249 8c047b66-80f2-bdae-70f3-23be71ff3dc6

--- src/edu/poly/bxmc/betaville/xml/UpdatedPreferenceWriter.java
+++ src/edu/poly/bxmc/betaville/xml/UpdatedPreferenceWriter.java
@@ -56,8 +56,9 @@ public class UpdatedPreferenceWriter {
 		if(System.getProperty("betaville.display.terrain.usegenerated")==null) System.setProperty("betaville.display.terrain.usegenerated", "false");
 		if(System.getProperty("betaville.sound.volume.master")==null) System.setProperty("betaville.sound.volume.master", "1.0");
 		
-		String cache_location = new String("/.betaville/cache/");
-		if(System.getProperty("betaville.cache.location")==null) System.setProperty("betaville.cache.location", new File(DriveFinder.getHomeDir().toString() + cache_location).toURI().toURL().toString());		
+		String cache_location =  new File(DriveFinder.getHomeDir().toString()) + "/.betaville/cache/";
+		// if(System.getProperty("betaville.cache.location")==null) System.setProperty("betaville.ca che.location", new File(DriveFinder.getHomeDir().toString() + cache_location).toURI().toURL().toString());		
+		if(System.getProperty("betaville.cache.location")==null) System.setProperty("betaville.cache.location", cache_location);		
 		System.out.println(" -->Writing default cache location:" + cache_location);
 		
 		if(System.getProperty("betaville.cache.size")==null) System.setProperty("betaville.cache.size", "300");
